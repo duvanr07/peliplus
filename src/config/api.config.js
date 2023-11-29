5718ad7ae474aa1107b65b7e25117ba6fb9f8600
Duvan Rosero <drosero@talenta.cloud>
2023-11-28 22:09:04 -0500

    refactor: remove endpoint test

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,12 +1,9 @@
 export const config = {
   host: "https://api.themoviedb.org/3",
   endpoints: {
-    pokemons: {
-      all: "/pokemon?limit=151",
-    },
     movies: {
       nowPlaying: "/movie/now_playing",
-      discover: "/discover/movie"
+      discover: "/discover/movie",
     },
     movie: {
       detail: "/movie/${id}",
